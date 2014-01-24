e4a818fed74ffe917c99971e1af36b8ff673e417
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-01-24 23:26:18 +0200

    Added metafiles

--- Assets/Scripts/KauhaTesti.cs
+++ Assets/Scripts/KauhaTesti.cs
@@ -13,10 +13,20 @@ public class KauhaTesti : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        //transform.position = new Vector3(transform.position.x, transform.position.y + Input.GetAxis("Mouse ScrollWheel") * 50 * Time.deltaTime, transform.position.z);
 	}
 
     void FixedUpdate()
     {
-        rigidbody.AddForce(0, Input.GetAxis("Fire1"), 0, ForceMode.Impulse); //Input.GetAxis("Mouse ScrollWheel");
+        //rigidbody.AddForce(0, Input.GetAxis("Fire1"), 0, ForceMode.Impulse); //Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetButton("Fire1"))
+        {
+            rigidbody.MovePosition(new Vector3(transform.position.x, transform.position.y + 1 * Time.deltaTime, transform.position.z));
+        }
+
+        else if (Input.GetButton("Fire1"))
+        {
+            rigidbody.MovePosition(new Vector3(transform.position.x, transform.position.y + 1 * Time.deltaTime, transform.position.z));
+        }
     }
 }
