a690b93a417717245567aa6b5796ca5a98950fb4
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-01-24 22:40:56 +0200

    Added placeholder models

---
+++ Assets/Scripts/KauhaTesti.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KauhaTesti : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(0, Input.GetAxis("Fire1"), 0, ForceMode.Impulse); //Input.GetAxis("Mouse ScrollWheel");
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform gameObject;
+    public Transform containerObject;
+    public float spawnInterval;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("Spawn", 0, spawnInterval);
+	}
+	
+	// Update is called once per frame
+	void Spawn()
+    {
+        var go = (Transform)Instantiate(gameObject, transform.position, transform.rotation);
+        go.transform.parent = containerObject;
+	}
+}
