ef2e2ab74d33e6d8dd0d9ac94398889381381e26
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-01-25 18:27:29 +0200

    Started implementing tree felling minigame

---
+++ Assets/Scripts/TreeFeller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeFeller : MonoBehaviour
+{
+    public int health = 100;
+    public int damage = 10;
+    public float coolDownTime = 2.0f;
+    public GUIText healthText;
+
+    private float coolDownTimer = 0;
+    private Animation animation;
+
+	// Use this for initialization
+	void Start()
+    {
+        animation = GetComponent<Animation>();
+	}
+
+    void Update()
+    {
+        coolDownTimer -= Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (coolDownTimer <= 0)
+        {
+            animation.Play();
+            health -= damage;
+            coolDownTimer = coolDownTime;
+		 }       
+    }
+
+    void OnGUI()
+    {
+        healthText.text = "Tree health: " + health;
+    }
+}
