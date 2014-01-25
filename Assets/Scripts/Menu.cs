e327f0a43f6021240669423da95f32c0025af505
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-01-26 01:48:42 +0200

    Added basic menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    public GUISkin skin;
+
+    void Start()
+    {
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = skin;
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 100, 300, 200),
+            "Heitähä vähä löylyä\n(Start Game)"))
+            Application.LoadLevel("testi");
+    }
+}
