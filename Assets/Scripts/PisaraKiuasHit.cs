2b5b3853c7f120866de8ce465e0e44959f51e863
NoobaMies <nooba.san@gmail.com>
2014-01-25 16:32:24 +0200

    Tshhh added
    TSHHHHHHHHHHHHH~!

--- Assets/Scripts/PisaraKiuasHit.cs
+++ Assets/Scripts/PisaraKiuasHit.cs
@@ -5,6 +5,7 @@ public class PisaraKiuasHit : MonoBehaviour
 {
     public GUIText score;
     public ParticleSystem steamEmitter;
+    public GameObject soundEmmitter;
     private int hits;
 
     void Start()
@@ -18,9 +19,12 @@ public class PisaraKiuasHit : MonoBehaviour
         {
             hits++;
 
-            var go = (ParticleSystem)Instantiate(steamEmitter, other.transform.position, steamEmitter.transform.rotation);
+            var steam = (ParticleSystem)Instantiate(steamEmitter, other.transform.position, steamEmitter.transform.rotation);
+            var tshhh = (GameObject)Instantiate(soundEmmitter, other.transform.position, soundEmmitter.transform.rotation);
+            tshhh.GetComponent<AudioSource>().Play();
             Destroy(other.gameObject);
-            Destroy(go, 10.0f);
+            Destroy(steam, 10.0f);
+            Destroy(tshhh, 5.0f);
         }
     }
 
