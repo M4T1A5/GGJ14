087f86c98c3a9ab8cd7cd0c57a74149972ebd3c4
Eracage <eracage@gmail.com>
2014-01-25 13:39:38 +0200

    changed some scaling to global scaling, added stones to stone oven

---
+++ Assets/Scripts/randRotationNSize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randRotationNSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.Rotate(Random.rotation.eulerAngles);
+        //transform.localScale = new Vector3(
+        //    Random.Range(0.8f, 1.2f),
+        //    Random.Range(0.8f, 1.2f),
+        //    Random.Range(0.8f, 1.2f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
