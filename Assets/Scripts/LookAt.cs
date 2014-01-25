a0f34e12a35c479d820492d461a5b0bccd050014
Eracage <eracage@gmail.com>
2014-01-26 01:58:33 +0200

    made camera follow kauha head

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour 
+{
+    public GameObject LookTarget;
+
+    private Quaternion startRotation;
+	// Use this for initialization
+	void Start () 
+    {
+        startRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.LookAt(LookTarget.transform);
+        //Quaternion otherRotation = transform.localRotation;
+
+        //transform.localRotation = startRotation;
+        //transform.localRotation = otherRotation * startRotation;
+        
+	}
+}
